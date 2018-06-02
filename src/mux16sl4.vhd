3d71792d2050eb48967f461e818b89e8b2e93281
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2018-06-02 09:23:16 -0300

    correção de dimensão do barramento

--- src/mux16sl4.vhd
+++ src/mux16sl4.vhd
@@ -5,19 +5,19 @@ use ieee.numeric_std.all;
 entity mux16sl4 is
 	port(
 		sel      : in unsigned(1 downto 0);
-		data_in0 : in unsigned(16 downto 0);
-		data_in1 : in unsigned(16 downto 0);
-		data_in2 : in unsigned(16 downto 0);
-		data_in3 : in unsigned(16 downto 0);
-		data_out : out unsigned(16 downto 0)
+		data_in0 : in unsigned(15 downto 0);
+		data_in1 : in unsigned(15 downto 0);
+		data_in2 : in unsigned(15 downto 0);
+		data_in3 : in unsigned(15 downto 0);
+		data_out : out unsigned(15 downto 0)
 	);
 end entity;
 
 architecture a_mux16sl4 of mux16sl4 is
 begin
-	data_out <= data_in0 when sel='00' else
-				data_in1 when sel='01' else
-				data_in2 when sel='10' else
-				data_in3 when sel='11' else
+	data_out <= data_in0 when sel="00" else
+				data_in1 when sel="01" else
+				data_in2 when sel="10" else
+				data_in3 when sel="11" else
 				"0000000000000000";
 end architecture;
