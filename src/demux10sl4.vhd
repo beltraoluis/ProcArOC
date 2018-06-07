0b3f5110c0b76f793c1a627c461bad7af8323d84
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2018-06-06 21:16:26 -0300

    correção dos mux de 13bits

--- src/demux10sl4.vhd
+++ src/demux10sl4.vhd
@@ -15,12 +15,12 @@ end entity;
 
 architecture a_demux10to4 of demux10to4 is
 begin
-	data_out0 <= data_in when sel='00' else
+	data_out0 <= data_in when sel="00" else
 				 "00000000000";
-	data_out1 <= data_in when sel='01' else
+	data_out1 <= data_in when sel="01" else
 				 "00000000000";
-	data_out2 <= data_in when sel='10' else
+	data_out2 <= data_in when sel="10" else
 				 "00000000000";
-	data_out3 <= data_in when sel='11' else
+	data_out3 <= data_in when sel="11" else
 				 "00000000000";
 end architecture;
