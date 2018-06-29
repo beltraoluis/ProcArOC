08d0182dd47918e12a040ebb95c958338682be0b
Luís Henrique Beltrão Santana <beltraoluis@gmail.com>
2018-06-28 23:35:16 -0300

    fim asm

--- validaPIC.asm
+++ validaPIC.asm
@@ -1,28 +1,56 @@
-
-
-
-
-
-	inicio: ; preenchendo a ram com 32 valores correspondentes aos endereços
-			MOVLW D'4'        ; 4 -> w
+			MOVLW 1           ; 1 -> w
 			MOVWF FSR         ; w -> fsr
-			MOVWF INDF        ; w -> indf[fsr]
-	ram:	
-			INCF FSR          ; fsr++
+			CLRF INDF         ; 0 -> indf[fsr]
+	ram:	INCF FSR          ; fsr++
 			MOVF FSR, 0       ; fsr -> w
 			MOVWF INDF        ; w -> indf[fsr]
-			SUBLW D'32'       ; 32 - w -> w
+			SUBLW 32          ; 32 - w -> w
 			BTFSS STATUS,2    ; pula a proxima instrução se a ultima operação resultou em 0
 			GOTO ram
-			MOVLW D'4'        ; 4 -> w
+			MOVLW 2           ; 2 -> w
 			MOVWF FSR         ; w -> fsr
-	crivo2:
-			CLRF INDF         ; 0 -> indf[fsr]
+	crivo2: CLRF INDF         ; 0 -> indf[fsr]
 			INCF FSR          ; fsr++
 			INCF FSR          ; fsr++
-			MOVLW D'32'       ; 32 -> w
+			MOVLW 32          ; 32 -> w
 			SUBWF FSR, 0      ; fsr - w -> w
 			MOVWF $1          ; w -> $1
-			BTFSC $1,7        ; pula a proxima instrução se
+			BTFSC $1,7        ; pula a proxima instrução se fsr > w
 			GOTO crivo2
-	crivo3:
+			MOVLW 3           ; 3 -> w
+			MOVWF FSR         ; w -> fsr
+	crivo3: CLRF INDF         ; 0 -> indf[fsr]
+			INCF FSR          ; fsr++
+			INCF FSR          ; fsr++
+			INCF FSR          ; fsr++
+			MOVLW 32          ; 32 -> w
+			SUBWF FSR, 0      ; fsr - w -> w
+			MOVWF $1          ; w -> $1
+			BTFSC $1,7        ; pula a proxima instrução se fsr > w
+			GOTO crivo3
+			MOVLW 5           ; 5 -> w
+			MOVWF FSR         ; w -> fsr
+	crivo5: CLRF INDF         ; 0 -> indf[fsr]
+			MOVLW 5           ; 5 -> w
+			ADDWF FSR,1       ; w + fsr -> fsr
+			MOVLW 32          ; 32 -> w
+			SUBWF FSR, 0      ; fsr - w -> w
+			MOVWF $1          ; w -> $1
+			BTFSC $1,7        ; pula a proxima instrução se fsr > w
+			GOTO crivo5
+	loop:	MOVLW 0           ; 0 -> w
+			MOVWF FSR         ; w -> fsr
+	loop2:  INCF FSR          ; fsr++
+			MOVLW 0           ; 0 -> w
+			SUBWF INDIF,0	  ; indf - 0 -> w
+			BTFSS STATUS,2    ; pula a proxima instrução se a ultima operação resultou em 0
+			GOTO show
+			MOVLW 32          ; 32 -> w
+			SUBWF FSR, 0      ; fsr - w -> w
+			MOVWF $1          ; w -> $1
+			BTFSC $1,7        ; pula a proxima instrução se fsr > w
+			GOTO loop2
+			GOTO loop
+	show	MOVF INDF,0       ; indif -> w
+			MOVWF PORTB		  ; w -> portb
+			GOTO loop2
